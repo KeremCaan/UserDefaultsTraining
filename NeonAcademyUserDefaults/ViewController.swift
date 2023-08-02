40da8f668ab0121c51aeec50f2b527caee45bb43
KeremCaan <keremcaan@hotmail.com>
2023-08-02 08:55:48 +0300

    User Defaults Training

--- NeonAcademyUserDefaults/ViewController.swift
+++ NeonAcademyUserDefaults/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var switchState: UISwitch!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var stepperLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stepperLabel.text = String(defaults.object(forKey: "manyTimesVisited") as? Int ?? 0)
+        textField.text = defaults.object(forKey: "placeName") as? String
+        switchState.isOn = defaults.object(forKey: "isVisited") as? Bool ?? false
+        stepper.value = Double(defaults.object(forKey: "manyTimesVisited") as? Double ?? 0)
+        }
+
+    let defaults = UserDefaults.standard
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        let stepperValue = sender.value
+        stepperLabel.text = "\(Int(stepperValue))"
     }
-
-
+    @IBAction func saveButton(_ sender: UIButton) {
+        defaults.set(switchState.isOn, forKey: "isVisited")
+        defaults.set(textField.text, forKey: "placeName")
+        defaults.set(Int(stepperLabel.text!), forKey: "manyTimesVisited")
+    }
+    
+    
 }
 
